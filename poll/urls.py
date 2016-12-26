147725ff7bd8645798a4b0bdeddbc950a1166e80
flavio <flaviomargarito@gmail.com>
2016-12-26 16:40:18 -0200

    Alterando as urls para usar as generic views.

--- poll/urls.py
+++ poll/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.results, name='results'),
 
     # ex: /poll/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
