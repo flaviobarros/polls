79274b1dfaa44ff018535243ad63fe0798d589a1
flavio <flaviomargarito@gmail.com>
2016-12-26 16:47:00 -0200

    Alterando o views para as classes.

--- poll/urls.py
+++ poll/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # ex: /polls/5/
-    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # ex: /polls/5/results/
-    url(r'^(?P<pk>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
     # ex: /poll/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
